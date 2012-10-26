41752648596139f91235e8864a2deeb369f6d856
?????? ?. <?????? ?.@localhost>
2012-10-27 02:46:00 +0400

    ????????? ??????? ?????.

---
+++ Lexer/lexer2/parcer_tab.h
@@ -0,0 +1,44 @@
+typedef union
+{
+	int uInt;
+	char* uId;
+	char* uString;
+	struct ExprNode* uExpr;
+	struct ExprSeqNode* uExprSeq;
+	struct StmtNode* uStmt;
+	struct StmtSeqNode* uStmtSeq;
+	struct MethodDefParamSeqNode* uMethodDefParamSeq;
+	struct MethodDefParamNode* uMethodDefParam;
+	struct ProgramNode* uProgram;
+} YYSTYPE;
+#define	INT	258
+#define	STRING	259
+#define	EOL	260
+#define	DEF	261
+#define	END	262
+#define	WHILE	263
+#define	DO	264
+#define	UNTIL	265
+#define	CLASS	266
+#define	SELF	267
+#define	SUPER	268
+#define	RETURN	269
+#define	ID	270
+#define	SPACE_BRACKET	271
+#define	EQUAL	272
+#define	NOTEQUAL	273
+#define	OR	274
+#define	AND	275
+#define	NIL	276
+#define	TRUE	277
+#define	FALSE	278
+#define	IF	279
+#define	ELSIF	280
+#define	ELSE	281
+#define	UNLESS	282
+#define	THEN	283
+#define	UMINUS	284
+#define	UBR	285
+
+
+extern YYSTYPE yylval;
---
+++ Lexer/lexer2/structures.h
@@ -0,0 +1,111 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+// Тип выражения
+enum ExprNodeType
+{
+	eInt,
+	eBool,
+	eString,
+	eId,
+	eAssign,
+	ePlus,
+	eMinus,
+	eMul,
+	eDiv,
+	eLess,
+	eMore,
+	eOr,
+	eAnd,
+	eEqu,
+	eNEqu,
+	eNot,
+	eUMinus,
+	eBrackets,
+	eQBrackets,
+	eQBracketsInit,
+	eFieldAcc,
+	eSuper,
+	eSelf,
+	eNil,
+};
+
+// Тип оператора
+enum StmtNodeType
+{
+	eExpr,
+	eIf,
+	eWhile,
+	eUnless,
+	eUntil,
+	eClassDef,
+	eMethodDef,
+	eReturn
+};
+
+// Выражение
+struct ExprNode
+{
+	enum ExprNodeType type; // Тип
+	
+	int value; // Числовое значение (или логическое)
+	char* id; // Имя
+	char* str; // Строка (для строковых литералов)
+	
+	struct ExprNode* left; // Левый операнд
+	struct ExprNode* right; // Правый операнд
+	struct ExprSeqNode* list; // Список выражений (для вызова функции)
+	
+	struct ExprNode* next; // Следующее в списке
+};
+
+// Последовательность выражений
+struct ExprSeqNode
+{
+	struct ExprNode* first; // Первый элемент
+	struct ExprNode* last; // Последний элемент
+};
+
+// Оператор
+struct StmtNode
+{
+	enum StmtNodeType type; // Тип
+	
+	struct ExprNode* expr; // Выражение
+	struct StmtSeqNode* block; // Блок операторов
+	
+	char* id; // Идентификатор (для объявлений классов и методов)
+	char* secondId; // Второй идентификатор (для наследования)
+	struct MethodDefParamSeqNode* params; // Параметры объявляемой функции
+	
+	struct StmtNode* next; // Следующий в списке
+};
+
+// Последовательность операторов
+struct StmtSeqNode
+{
+	struct StmtNode* first; // Первый
+	struct StmtNode* last; // Последний
+};
+
+// Последовательность параметров в объявлении функции
+struct MethodDefParamSeqNode
+{
+	struct MethodDefParamNode* first; // Первый
+	struct MethodDefParamNode* last;// Последний
+};
+
+// Параметр в объявлении фукнции
+struct MethodDefParamNode
+{
+	char* id; // Идентификатор
+	struct MethodDefParamNode* next; // Следующий в списке
+};
+
+// Корень дерева
+struct ProgramNode
+{
+	struct StmtSeqNode* body; // Тело всей программы
+};
+
+#endif 
