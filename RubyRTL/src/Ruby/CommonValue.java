ffb06688a63a65807ba593ae7c105202b979d9ba
astr0lit <astr0lit@POWER>
2012-12-15 18:06:14 +0400

    ????????? RTL. ?????????? ????? ? ???????.

---
+++ RubyRTL/src/Ruby/CommonValue.java
@@ -0,0 +1,16 @@
+package Ruby;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ZerG
+ */
+public class CommonValue {
+    public int intValue;
+    
+    public CommonClass objectValue;
+}
---
+++ RubyRTL/src/Ruby/Console.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ruby;
+
+/**
+ *
+ * @author astr0lit
+ */
+public class Console {
+    static public void printInt(int arg) {
+        System.out.print(arg);
+    }
+    static public void printString(String arg) {
+        System.out.print(arg);
+    }
+}
